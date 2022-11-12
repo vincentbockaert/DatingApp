431f0f26acbf6517eab6b7348c554cd0c0b94588
Vincent Bockaert <bockaert.vincent1997@gmail.com>
2022-11-12 01:07:16 +0100

    Init commit, end of section 2

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,47 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        public DataContext _context { get; }
+        public UsersController(DataContext context)
+        {
+            this._context = context;
+        }
+
+        // api/users - synchronously 
+        // [HttpGet]
+        // public ActionResult<IEnumerable<AppUser>> GetUsers()
+        // {
+        //     return _context.Users.ToList();
+        // }
+
+        // api/users
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        // // api/users/3 - synchronously 
+        // [HttpGet("{id}")]
+        // public ActionResult<AppUser> GetUser(int id)
+        // {
+        //     return _context.Users.Find(id);
+        // }
+
+
+         // api/users/3
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+    }
+}
